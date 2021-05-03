55f99e5cbcfcbb20d8f38034ba791c559c3510c7
KodaRayTominus <36204580+KodaRayTominus@users.noreply.github.com>
2021-05-03 15:57:54 -0700

    Update ClusterGrid.cs

--- ElevationExperiment/Export/Core/Grid/ClusterGrid.cs
+++ ElevationExperiment/Export/Core/Grid/ClusterGrid.cs
@@ -12,25 +12,32 @@ using Newtonsoft.Json;
 namespace Elevation
 {
 
-  public static class ClusterGrid{
-    
-    private List<List<Cluster>> Clusters = new List<List<Cluster>>();
-    
-    private int size;
-    
-    public ClusterGrid(int clusterDimentions){
-      
-      size = clusterDimentions;
+    public static class ClusterGrid{
+
+        private List<List<Cluster>> Clusters = new List<List<Cluster>>();
+
+        private int size;
+
+        public ClusterGrid(int clusterDimentions, int height, int width){
+
+            size = clusterDimentions * clusterDimentions;
+
+            for(int i = 0; i < size; i++){
+
+                for(int j = 0; j < size; j++){
+
+                }
+            }
+        }
+
+        public static List<List<Cluster> GetClusters(){
+
+            return this.Clusters;
+        }
+
+        public static void SetClusters(List<List<Cluster> clusters){
+
+            this.Clusters - clusters;
+        }
     }
-    
-    public static List<List<Cluster> GetClusters(){
-      
-      return this.Clusters;
-    }
-    
-    public static void SetClusters(List<List<Cluster> clusters){
-      
-      this.Clusters - clusters;
-    }
-  }
 }
