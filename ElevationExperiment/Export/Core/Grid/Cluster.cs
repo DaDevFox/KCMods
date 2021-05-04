08e6e3423ccdd877ed0c10a29c09857e6fb49f0d
Dakota Ray Tominus <koda.tominus@hotmail.com>
2021-05-04 00:50:19 -0700

    Setting up class for cluster possible routes.

--- ElevationExperiment/Export/Core/Grid/Cluster.cs
+++ ElevationExperiment/Export/Core/Grid/Cluster.cs
@@ -7,33 +7,37 @@ namespace Elevation
     public class Cluster : IEnumerable
     {
   
-    public Dictionary<string, Elevation.PrebakedPathfinder.Node> ClustersGrid;
+        public Dictionary<string, Elevation.PrebakedPathfinder.Node> ClustersGrid;
     
-    public Dictionary<string, Elevation.PrebakedPathfinder.Node> ClustersUpperGrid;
-    
-    public Cluster( Dictionary<string, Elevation.PrebakedPathfinder.Node> clustersGrid){
+        public Dictionary<string, Elevation.PrebakedPathfinder.Node> ClustersUpperGrid;
 
-        this.ClustersGrid = clustersGrid;
-    }
+        private static Dictionary<string, GridRoute> routes;
+
+        internal static Dictionary<string, GridRoute> Routes { get => routes; set => routes = value; }
+
+        public Cluster( Dictionary<string, Elevation.PrebakedPathfinder.Node> clustersGrid){
+
+            this.ClustersGrid = clustersGrid;
+        }
     
-    public void SetCluster(Dictionary<string, Elevation.PrebakedPathfinder.Node> clusterGrid, Dictionary<string, Elevation.PrebakedPathfinder.Node> clustesrGrid){
+        public void SetCluster(Dictionary<string, Elevation.PrebakedPathfinder.Node> clusterGrid, Dictionary<string, Elevation.PrebakedPathfinder.Node> clustesrGrid){
 
             this.ClustersGrid = clustesrGrid;
-    }
-    public void SetClustersUpper(Dictionary<string, Elevation.PrebakedPathfinder.Node> clustersUpperGrid){
+        }
+        public void SetClustersUpper(Dictionary<string, Elevation.PrebakedPathfinder.Node> clustersUpperGrid){
 
             this.ClustersUpperGrid = clustersUpperGrid;
-    }
+        }
     
-    public Dictionary<string, Elevation.PrebakedPathfinder.Node> GetCluster(){
+        public Dictionary<string, Elevation.PrebakedPathfinder.Node> GetCluster(){
       
-      return this.ClustersGrid;
-    }
+            return this.ClustersGrid;
+        }
     
-    public Dictionary<string, Elevation.PrebakedPathfinder.Node> GetClustersUpper(){
+        public Dictionary<string, Elevation.PrebakedPathfinder.Node> GetClustersUpper(){
       
-      return this.ClustersUpperGrid;
-    }
+            return this.ClustersUpperGrid;
+        }
 
         public IEnumerator GetEnumerator()
         {
---
+++ ElevationExperiment/Export/Core/Grid/GridRoute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevation
+{
+    class GridRoute
+    {
+        public string StartGridID;
+
+        public string ClusterGridID;
+
+        public string EndGridID;
+
+        public int ClusterRouteStart;
+
+        public int ClusterRouteEnd;
+
+        public GridRoute(int start, int end, string clusterID)
+        {
+            this.ClusterRouteStart = start;
+
+            this.ClusterRouteEnd = end;
+
+            this.ClusterGridID = clusterID;
+        }
+    }
+}
